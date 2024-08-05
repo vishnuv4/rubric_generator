c3991a5fa006e9221b6799c6164aa94c15a94b97
Vishnu Venkatesh <vishnuv@seas.upenn.edu>
2024-08-05 02:08:37 -0700

    added requirements.txt

--- setup.ps1
+++ setup.ps1
@@ -2,8 +2,5 @@ if (Test-Path .venv){
     Remove-Item -r .venv
 }
 python -m venv .venv
-.venv\Scripts\python.exe -m pip install tabulate
-.venv\Scripts\python.exe -m pip install pandas
-.venv\Scripts\python.exe -m pip install openpyxl
-.venv\Scripts\python.exe -m pip install pathlib
+.venv\Scripts\python.exe -m pip install -r requirements.txt
 activate .venv
